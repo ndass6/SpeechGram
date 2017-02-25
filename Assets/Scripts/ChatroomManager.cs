16456b19a3e92dfe5de7e7a4267089f1275853ee
Jonathan Chen <jonathanchen@gatech.edu>
2017-02-25 05:13:43 -0500

    Structural chatroom work (prefabs and scripts)

---
+++ Assets/Scripts/ChatroomManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatroomManager : MonoBehaviour
+{
+    public Message MessagePrefab;
+
+    private Transform messages;
+
+    public void Start()
+    {
+        messages = transform.FindChild("Messages");
+    }
+
+    public void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            AddMessage("Guy", "Test message");
+    }
+
+    public void AddMessage(string speaker, string message)
+    {
+        Message temp = Instantiate(MessagePrefab, transform.position, Quaternion.identity);
+        temp.SetMessage(speaker, message);
+
+        // TODO Offset tracking
+        temp.transform.SetParent(messages);
+    }
+}
---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Message : MonoBehaviour
+{
+    private string speaker;
+    public string Speaker
+    {
+        get { return speaker; }
+    }
+    private string text;
+    public string Text
+    {
+        get { return text; }
+    }
+
+    public void SetMessage(string speaker, string text)
+    {
+        this.speaker = speaker;
+        this.text = text;
+        GetComponent<Text>().text = "<b>" + speaker + "</b>: " + text;
+    }
+}
