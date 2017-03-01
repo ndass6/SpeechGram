46925e4ad3096f0525c127fb9d33cbbf3f359c44
Tejasvi (Teju) Nareddy <tejunareddy@gmail.com>
2017-03-01 09:52:51 -0500

    Initial setup of voice identification

---
+++ Assets/Scripts/VoiceIdentification.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.ProjectOxford.SpeakerRecognition;
+using Microsoft.ProjectOxford.SpeakerRecognition.Contract;
+using Microsoft.ProjectOxford.SpeakerRecognition.Contract.Identification;
+
+public class VoiceIdentification : MonoBehaviour {
+
+    private SpeakerIdentificationServiceClient serviceClient;
+
+    public VoiceIdentification()
+    {
+        serviceClient = new SpeakerIdentificationServiceClient("KEY_GOES_HERE");
+        // How do I not store my key in plain text in unity?!?!?!
+    }
+
+    void Start () {
+		// DEBUG on start
+	}
+	
+	void Update () {
+        // DEBUG on mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            // TODO
+        }
+    }
+}
