d190d5e293acd0932d9400917a1ecb3a91ce8851
Tejasvi (Teju) Nareddy <tejunareddy@gmail.com>
2017-03-03 13:31:27 -0500

    Initial layout for api calls

--- Assets/Scripts/VoiceIdentification.cs
+++ Assets/Scripts/VoiceIdentification.cs
@@ -7,13 +7,11 @@ using System.Net;
 
 public class VoiceIdentification : MonoBehaviour {
 
+    // API endpoints
+    public static readonly string PROFILE_ENDPOINT = "https://westus.api.cognitive.microsoft.com/spid/v1.0/identificationProfiles";
+
     // TODO storing the key in not plaintext!
     private string key = "KEY GOES HERE";
-
-    public VoiceIdentification()
-    {
-        
-    }
 
     void Start () {
 		// DEBUG on start
@@ -27,7 +25,7 @@ public class VoiceIdentification : MonoBehaviour {
         }
     }
 
-    public void EnrollUser(string id, Stream audioStream)
+    public void EnrollUser(Stream audioStream)
     {
         // TODO Create the user profile
 
@@ -35,7 +33,7 @@ public class VoiceIdentification : MonoBehaviour {
         Debug.Log("Created user profile");
 
         // TODO Call the enroll API
-        Debug.Log("Enrolling " + id);
+        Debug.Log("Enrolling");
 
         // TODO Return to the original caller while continuing this call to verify
         
@@ -54,4 +52,49 @@ public class VoiceIdentification : MonoBehaviour {
 
         // TODO call the callback
     }
+
+    #region API Methods
+
+    private void CreateProfile(Stream audioStream)
+    {
+        // Create the user profile by calling the api
+        // https://westus.dev.cognitive.microsoft.com/docs/services/563309b6778daf02acc0a508/operations/5645c068e597ed22ec38f42e
+
+        // Construct the www form
+        WWWForm form = new WWWForm();
+
+        // Call the api endpoint
+        WWW www = new WWW(PROFILE_ENDPOINT, form);
+        StartCoroutine(CreateProfileRequest(www, audioStream, HandleCreateProfile));
+    }
+
+    #endregion
+
+    #region API response handlers
+
+    private void HandleCreateProfile(string res, Stream audioStream)
+    {
+        // TODO figure out how to do real json parsing
+        string id = res.Substring(34, 36);
+    }
+
+    #endregion
+
+    #region API request coroutine
+
+    private IEnumerator CreateProfileRequest(WWW www, Stream audioStream, Action<string, Stream> callback)
+    {
+        yield return www;
+
+        if (www.error == null)
+        {
+            callback(www.text, audioStream);
+        }
+        else
+        {
+            Debug.Log("ERROR: " + www.error + "\n" + www.text);
+        }
+    }
+
+    #endregion
 }
